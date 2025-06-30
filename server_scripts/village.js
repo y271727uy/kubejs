d792dd61a56fd85b4bbc2c27fa2c084f83f16a41
y271727uy <145999682+y271727uy@users.noreply.github.com>
2025-06-30 15:59:49 +0800

    kubejs change

---
+++ server_scripts/village.js
@@ -0,0 +1,16 @@
+MoreJSEvents.updateVillagerOffers(event => {
+    event.getOffers().forEach(o => {
+        let first_input = o.getFirstInput()
+        if (first_input.is('emerald')) {
+            o.setFirstInput(`${first_input.getCount()}x kubejs:silver_gt_credit`)
+        }
+        let second_input = o.getSecondInput()
+        if (second_input.is('emerald')) {
+            o.setSecondInput(`${second_input.getCount()}x kubejs:silver_gt_credit`)
+        }
+        let output = o.getOutput()
+        if (output.is('emerald')) {
+            o.setOutput(`${output.getCount()}x kubejs:silver_gt_credit`)
+        }
+    })
+})
