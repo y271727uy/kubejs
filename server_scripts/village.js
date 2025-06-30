233fd6c71d57a008e32143bad09961894f9674ae
y271727uy <145999682+y271727uy@users.noreply.github.com>
2025-06-30 17:18:38 +0800

    Update village.js

--- server_scripts/village.js
+++ server_scripts/village.js
@@ -14,3 +14,37 @@ MoreJSEvents.updateVillagerOffers(event => {
         }
     })
 })
+
+MoreJSEvents.updateWandererOffers(event => {
+    event.getOffers().forEach(o => {
+        let first_input = o.getFirstInput()
+        if (first_input.is('emerald')) {
+            o.setFirstInput(`${first_input.getCount()}x kubejs:silver_gt_credit`)
+        }
+        let second_input = o.getSecondInput()
+        if (second_input.is('emerald')) {
+            o.setSecondInput(`${second_input.getCount()}x kubejs:silver_gt_credit`)
+        }
+        let output = o.getOutput()
+        if (output.is('emerald')) {
+            o.setOutput(`${output.getCount()}x kubejs:silver_gt_credit`)
+        }
+    })
+})
+
+MoreJSEvents.updateAbstractVillagerOffers(event => {
+    event.getOffers().forEach(o => {
+        let first_input = o.getFirstInput()
+        if (first_input.is('emerald')) {
+            o.setFirstInput(`${first_input.getCount()}x kubejs:silver_gt_credit`)
+        }
+        let second_input = o.getSecondInput()
+        if (second_input.is('emerald')) {
+            o.setSecondInput(`${second_input.getCount()}x kubejs:silver_gt_credit`)
+        }
+        let output = o.getOutput()
+        if (output.is('emerald')) {
+            o.setOutput(`${output.getCount()}x kubejs:silver_gt_credit`)
+        }
+    })
+})
